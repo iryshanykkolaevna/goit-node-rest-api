d00dc16851df8a5803cadf50cbbb4041d5cce9c0
iryshanykkolaevna <141450700+iryshanykkolaevna@users.noreply.github.com>
2024-05-12 12:03:36 +0300

    push 12.05-12.03

---
+++ schemas/contactsSchemas.js
@@ -0,0 +1,13 @@
+import Joi from "joi";
+
+export const createContactSchema = Joi.object({
+  name: Joi.string().required().min(2),
+  email: Joi.string().email().required(),
+  phone: Joi.string().required().min(7),
+});
+
+export const updateContactSchema = Joi.object({
+  name: Joi.string().min(2),
+  email: Joi.string().email(),
+  phone: Joi.string().min(7),
+});
